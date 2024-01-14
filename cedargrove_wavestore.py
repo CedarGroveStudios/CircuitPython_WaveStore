253e02df2d26f65503284ecd2fad3032ad586ee4
Cedar Grove Maker Studios <jan@goolsbey.net>
2024-01-13 20:53:43 -0800

    initial repo load

--- cedargrove_wavestore.py
+++ cedargrove_wavestore.py
@@ -1,22 +1,16 @@
 # SPDX-FileCopyrightText: Copyright (c) 2024 JG for Cedar Grove Maker Studios
-#
 # SPDX-License-Identifier: MIT
 """
 `cedargrove_wavestore`
 ================================================================================
 
-A CircuitPython class to create and manage a collection of synthio wave_tables.
-
+A CircuitPython class to create and manage a file-based collection of synthio
+wave_tables.
 
 * Author(s): JG
 
 Implementation Notes
 --------------------
-
-**Hardware:**
-
-.. todo:: Add links to any specific hardware product page(s), or category page(s).
-  Use unordered list & hyperlink rST inline format: "* `Link Text <url>`_"
 
 **Software and Dependencies:**
 * ulab for CircuitPython
@@ -24,6 +18,8 @@ Implementation Notes
   https://circuitpython.org/downloads
 """
 
+import displayio
+import os
 import sdcardio
 import storage
 import ulab.numpy as np
@@ -51,50 +47,75 @@ class WaveStore:
         self._cs_pin = cs_pin
         self._debug = debug
 
+        self.printd("WaveStore class initializing...")
+
         sd_card = sdcardio.SDCard(self._spi_pins, self._cs_pin)
-        vfs = storage.VfsFat(sd_card)
-        storage.mount(vfs, "/sd")
+        self._vfs = storage.VfsFat(sd_card)
+        storage.mount(self._vfs, "/sd")
 
-    def capture_screen_to_file(self, display, filename="screenshot"):
-        self.printd(f"/sd/{filename}.bmp : Taking Screenshot...")
-        save_pixels("/sd/" + filename + ".bmp", display)
-        print('... Screenshot Taken')
+        self.printd("WaveStore init: SD card mounted")
 
-    def capture_bitmap_to_file(self, bitmap, filename="screenshot"):
-        self.printd(f"/sd/{filename}.bmp : Taking Screenshot...")
-        save_pixels("/sd/" + filename + ".bmp", bitmap)
-        print('... Screenshot Taken')
+    def write_screen_to_file(self, display, filename="screenshot.bmp"):
+        """Write the screen contents to a .bmp file."""
+        filename = f"/sd/{filename}"
+        self.printd(f"write_screen_to_file: {filename}")
+        save_pixels(filename, display)
+        print("... Screenshot Stored")
+
+    def write_bitmap_to_file(self, bitmap, filename="bitmap.bmp"):
+        """Write a bitmap image to a .bmp file."""
+        filename = f"/sd/{filename}"
+        self.printd(f"write_bitmap_to_file: {filename}")
+        save_pixels(filename, bitmap)
+        print("... Bitmap Stored")
+
+    def read_bitmap_from_file(self, filename="bitmap.bmp"):
+        """Read a .bmp file and return the bitmap as a TileGrid object."""
+        filename = f"/sd/{filename}"
+        self.printd(f"read_bitmap_from_file: {filename}")
+        image = displayio.OnDiskBitmap(filename)
+
+        # Create a TileGrid to hold the bitmap and return
+        # to be appended to a displayio.Group object
+        return displayio.TileGrid(image, pixel_shader=image.pixel_shader)
 
     def read_waveform(self, filename):
         """Reads entire wave file."""
-        self.printd(f"/sd/{filename} : Reading Waveform...")
-        with adafruit_wave.open(f"/sd/{filename}.wav") as w:
+        filename = f"/sd/{filename}"
+        self.printd(f"read_waveform: {filename} ")
+        with adafruit_wave.open(filename) as w:
             if w.getsampwidth() != 2 or w.getnchannels() != 1:
                 raise ValueError("read_waveform: unsupported format")
-            return memoryview(w.readframes(w.getnframes())).cast('h')
+            return memoryview(w.readframes(w.getnframes())).cast("h")
 
     def read_waveform_ulab(self, filename):
-        """Read wave file and make it lerp()-able to mix with another wave."""
-        self.printd(f"/sd/{filename} : Reading Waveform (ulab) ...")
-        with adafruit_wave.open(f"/sd/{filename}.wav") as w:
+        """Read wave file and make it lerp()-able to mix with another wave.
+        Usage example: my_wave = read_waveform_ulab("AKWF_0001.wav")"""
+        filename = f"/sd/{filename}"
+        self.printd(f"read_waveform_ulab: {filename} ")
+        with adafruit_wave.open(filename) as w:
             if w.getsampwidth() != 2 or w.getchannels() != 1:
                 raise ValueError("read_waveform_ulab: unsupported format")
             return np.frombuffer(w.readframs(w.getnframes()), dtype=np.int16)
 
-    # usage example
-    # my_wave = read_waveform("AKWF_granular_0001.wav")
-
     def write_waveform(self, wave_table, filename):
         """Formats and writes wave_table into a standard .wav file."""
-        self.printd(f"/sd/{filename} : Writing Waveform...")
-        with adafruit_wave.open(f"/sd/{filename}.wav", "W") as w:
+        filename = f"/sd/{filename}"
+        self.printd(f"write_waveform: {filename} ")
+        with adafruit_wave.open(f"/sd/{filename}", mode="w") as w:
             pass
 
-    def get_catalog(self):
-        """Looks at the SD card file system and returns a list of .wav files
-        and a list of .bmp files."""
-        self.printd("Reading Catalog...")
+    def get_catalog(self, path="/sd"):
+        """Looks at the SD card file system and returns a list of files in the root directory."""
+        self.printd(f"get_catalog: retrieving file list")
+        catalog = [
+            file
+            for file in os.listdir(path)
+            if not (os.stat(path + "/" + file)[0] & 0x4000)
+        ]
+        return catalog
 
     def printd(self, string):
+        """Print string contents if debug is True."""
         if self._debug:
             print(string)
