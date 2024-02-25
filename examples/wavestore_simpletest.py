114ebbbe3db00e3531a8d128d6b11a7194147ec7
Cedar Grove Maker Studios <jan@goolsbey.net>
2024-02-24 16:33:34 -0800

    testing filter object plotting and storing

--- examples/wavestore_simpletest.py
+++ examples/wavestore_simpletest.py
@@ -6,6 +6,7 @@
 import gc
 import time
 import board
+from math import log, cos, sin, sqrt, pi
 import displayio
 import synthio
 import adafruit_ili9341
@@ -15,6 +16,9 @@ from cedargrove_waveviz import WaveViz
 from cedargrove_wavestore import WaveStore
 
 DEBUG = False
+SAMP_RATE = 22_050  # samples per second
+
+synth = synthio.Synthesizer(sample_rate=SAMP_RATE)
 
 # Instantiate WaveStore to manage SD card contents
 w_store = WaveStore(board.SPI(), board.D20, debug=DEBUG)
@@ -43,7 +47,6 @@ harp_tone = [
 harp = WaveBuilder(oscillators=harp_tone, table_length=512)
 harp_icon = WaveViz(harp.wave_table, 10, 10, 128, 128)
 
-
 chime_tone = [
     (WaveShape.Sine, 1.00, -0.60),
     (WaveShape.Sine, 2.76, 0.20),
@@ -64,6 +67,64 @@ string_envelope = synthio.Envelope(
     sustain_level=0.500,
 )
 
+# Create three filters for testing
+lp_filter = synth.low_pass_filter(880)
+hp_filter = synth.high_pass_filter(440)
+bp_filter = synth.band_pass_filter(440)
+
+print(f"lp_filter: {lp_filter}")
+print(lp_filter[0])
+print([param for param in lp_filter])
+print(f"hp_filter: {hp_filter}")
+print(f"bp_filter: {bp_filter}")
+
+
+# audio cookbook: https://github.com/shepazu/Audio-EQ-Cookbook/blob/master/Audio-EQ-Cookbook.txt
+# w0 = 2*pi*f0/Fs  # ???
+
+# from https://www.musicdsp.org/en/latest/Analysis/186-frequency-response-from-biquad-coefficients.html
+# w = frequency (0 < w < PI)
+# y = 20*log((sqrt(square(a0*square(cos(w))-a0*square(sin(w))+a1*cos(w)+a2)+square(2*a0*cos(w)*sin(w)+a1*(sin(w))))/
+#          sqrt(square(square(cos(w))-square(sin(w))+b1*cos(w)+b2)+square(2*cos(w)*sin(w)+b1*(sin(w))))));
+
+
+# square(x) = x*x
+def sq(x):
+    return x * x
+
+
+def biquad_y(w0):
+    # w0: frequency for transfer function; 0 to pi ???
+    y = 20 * log(
+        (
+            sqrt(
+                sq(a0 * sq(cos(w0)) - a0 * sq(sin(w0)) + a1 * cos(w0) + a2)
+                + sq(2 * a0 * cos(w0) * sin(w) + a1 * (sin(w0)))
+            )
+            / sqrt(
+                sq(sq(cos(w0)) - sq(sin(w0)) + b1 * cos(w0) + b2)
+                + sq(2 * cos(w0) * sin(w0) + b1 * (sin(w0)))
+            )
+        )
+    )
+    return y
+
+
+a0 = 1
+a1 = lp_filter[0]
+a2 = lp_filter[1]
+b0 = lp_filter[2]
+b1 = lp_filter[3]
+b2 = lp_filter[4]
+
+for i in range(1, 100, 4):
+    w = pi / i  # ???
+    y0 = biquad_y(w)
+    print(f"i, w, y0 : {i:03.0f} {w:08.4f} {y0:+08.4f}")
+
+while True:
+    pass
+
 # Test 1: Get the SD directory and print list to REPL
 print("\nTest 1: Get the SD directory and print list to REPL")
 print(f"SD directory: {w_store.get_catalog()}")
@@ -71,7 +132,9 @@ print(" completed")
 
 # Test 2: Write bitmap image to a file
 print("\nTest 2: Write bitmap image to a file")
-w_store.write_bitmap(harp_icon.bitmap, harp_icon.pixel_shader, filename="harp_icon.bmp", overwrite=True)
+w_store.write_bitmap(
+    harp_icon.bitmap, harp_icon.pixel_shader, filename="harp_icon.bmp", overwrite=True
+)
 print(" completed")
 
 # Test 3: Read and display saved bitmap
@@ -96,7 +159,9 @@ print(" completed")
 # Test 6: Write wave table to a file
 print("\nTest 6: Write wave table to a file")
 print(harp_icon.wave_table)
-w_store.write_wavetable(harp_icon.wave_table, "harp.wav", overwrite=True)
+w_store.write_wavetable(
+    harp_icon.wave_table, "harp.wav", samp_rate=SAMP_RATE, overwrite=True
+)
 print(" completed")
 
 # Test 7: Read wavetable as memory_view object from a file and display
@@ -124,7 +189,11 @@ print(" completed")
 
 # Test 13: Display wave table bitmap with transparency
 print("\nTest 13: Display wave table bitmap with transparency")
-splash.append(displayio.TileGrid(harp_icon.bitmap, pixel_shader=harp_icon.pixel_shader, x=100, y=100))
+splash.append(
+    displayio.TileGrid(
+        harp_icon.bitmap, pixel_shader=harp_icon.pixel_shader, x=100, y=100
+    )
+)
 print(" completed")
 
 # All tests completed
